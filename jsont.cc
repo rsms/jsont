rsms/jsont

commit dc53e74b841b43c01d715109efcb2073955a58d5

Fixed a bug with the C++ tokenizer where an array which ends in a number would not cause an ArrayEnd token to be emitted

jsont.cc
@@ -380,6 +380,14 @@ const Token& Tokenizer::next() {
                       _input.bytes[_value.offset] == '+') ) {
                   return setError(MalformedNumberLiteral);
                 }
+
+                // if we got terminated by a comma, set tok. else rewind
+                if (b == ',') {
+                  _token = _Comma;
+                } else {
+                  --_input.offset;
+                }
+
                 _value.length = _input.offset - _value.offset - 1;
                 return setToken(token);
               }

commit 249bdd70127dc2c1480234fa76651d5c73c0e841

Fixes a bug introduced by dc53e74b841b43c01d715109efcb2073955a58d5 where an illegal comma would silently be accepted and ignored.

jsont.cc
@@ -400,12 +400,8 @@ const Token& Tokenizer::next() {
                   return setError(MalformedNumberLiteral);
                 }
 
-                // if we got terminated by a comma, set tok. else rewind
-                if (b == ',') {
-                  _token = _Comma;
-                } else {
-                  --_input.offset;
-                }
+                // rewind the byte that terminated this number literal
+                --_input.offset;
 
                 _value.length = _input.offset - _value.offset - 1;
                 return setToken(token);